f6c6728bd9cb6019ad2c4fae5eb77566b3da5892
Kai Yamada <gpioblink@aol.com>
2019-06-27 10:48:50 +0900

    init firebase

--- front/minna-no-akizuki-vuetify/src/main.js
+++ front/minna-no-akizuki-vuetify/src/main.js
@@ -4,8 +4,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyD2eNVsaaaYwBeFBlMGblbFqc8jZRnbRl8',
+  authDomain: 'fir-vue-chat-8092e.firebaseapp.com',
+  databaseURL: 'https://fir-vue-chat-8092e.firebaseio.com',
+  projectId: 'fir-vue-chat-8092e',
+  storageBucket: 'fir-vue-chat-8092e.appspot.com',
+  messagingSenderId: '373785443513',
+  appId: '1:373785443513:web:97f43272560046e4'
+}
+firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
